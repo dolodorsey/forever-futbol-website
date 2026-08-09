c4ba8f68d05a678005a9fdcba8a745821e13bafe
dolodorsey <dolodorsey@gmail.com>
2026-08-08 20:13:18 -0400

    Hide public annual collection rosters

--- src/components/MuseumViews.tsx
+++ src/components/MuseumViews.tsx
@@ -1,31 +1,15 @@
 "use client";
 
-import { MuseumPage, museumConfigs } from "./MuseumPortfolio";
-import { MuseumEnhancements } from "./MuseumEnhancements";
-
-function EnhancedMuseum({ slug }: { slug: "living-legends" | "fallen-stars" | "women-make-the-world-go-round" }) {
-  const config = museumConfigs[slug];
-  return <>
-    <MuseumPage config={config} />
-    <MuseumEnhancements
-      museumSlug={config.slug}
-      museumName={config.name}
-      honorees={config.honorees}
-      cities={config.cities}
-      accent={config.accent}
-      accent2={config.accent2}
-    />
-  </>;
-}
+import { LivingLegendsPublic, FallenStarsPublic, WomenPublic } from "./MuseumPublic";
 
 export function LivingLegendsView() {
-  return <EnhancedMuseum slug="living-legends" />;
+  return <LivingLegendsPublic />;
 }
 
 export function FallenStarsView() {
-  return <EnhancedMuseum slug="fallen-stars" />;
+  return <FallenStarsPublic />;
 }
 
 export function WomenMuseumView() {
-  return <EnhancedMuseum slug="women-make-the-world-go-round" />;
+  return <WomenPublic />;
 }
